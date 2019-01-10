5cba829a28eddf4861162831f7916a32747c931d
chrisxf <info@sethorax.com>
2019-01-10 20:42:54 +0100

    Fixed bug if received data is not 16 bit long

--- lirc.go
+++ lirc.go
@@ -2,6 +2,7 @@ package lirc
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/hex"
 	"errors"
 	"log"
@@ -110,7 +111,19 @@ func reader(router *Router) {
 				state = REPLY
 			} else {
 				r := strings.Split(line, " ")
-				c, err := hex.DecodeString(r[0])
+				h := r[0]
+
+				if len(h) != 16 {
+					var buf bytes.Buffer
+					for i := len(h); i < 16; i++ {
+						buf.WriteString("0")
+					}
+					buf.WriteString(h)
+
+					h = buf.String()
+				}
+
+				c, err := hex.DecodeString(h)
 				if err != nil {
 					log.Println("Invalid lirc broadcats message received - code not parseable")
 					continue
